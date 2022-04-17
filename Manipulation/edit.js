d211257042b51bdcf0af1f74c2771cd7dc86783a
m926n810 <m926n810@ku.edu>
2022-04-17 18:27:39 -0500

    basically done

--- Manipulation/edit.js
+++ Manipulation/edit.js
@@ -12,6 +12,13 @@ function submit(){
     let textbox = document.getElementById("txtbx")
     let color = "rgb("+red+","+green+","+blue+")";
     let brcolor = "rgb("+brred+","+brgreen+","+brblue+")";
+    if(red<0 || red>255 ||green<0 || green>255 ||blue<0 || blue>255 ||
+        brred<0 || brred>255 ||brgreen<0 || brgreen>255 ||brblue<0 || brblue>255)
+        {
+            alert("One of the color value is out of rgb range 0-255");
+            return;
+        }
+    if(!width.includes("px")) width+="px";
     textbox.style.borderColor= color;
     textbox.style.borderWidth= width;
     textbox.style.backgroundColor= brcolor;
